05d613fa5186e3a34ce74c4b4ba4b48c8f947c92
Phuc <cauvang2508mine@gmail.com>
2026-07-15 07:34:12 +0700

    Fix ModuleNotFoundError for uwir in precompute script

--- scripts/precompute_physics.py
+++ scripts/precompute_physics.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 import argparse
 from multiprocessing import Pool
 import numpy as np
